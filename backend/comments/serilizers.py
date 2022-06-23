fbb4636bd1742bc80f5c3c7f3d9346476c50ea17
LizzieDworsky <amydworsky15@gmail.com>
2022-06-23 13:37:51 -0600

    created the CommentSerializer + imported everything I know I'll need from restframework and wrote the first views function, get_all_comments

---
+++ backend/comments/serilizers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from pyexpat import model
+from rest_framework import serializers
+from .models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "video_id", "text", "likes", "dislikes", "user_id"]
+        depth = 1
