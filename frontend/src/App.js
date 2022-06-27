c8aaf753b2628a784a68fee2f2bfb2ac5a432c8a
LizzieDworsky <amydworsky15@gmail.com>
2022-06-27 15:41:33 -0600

    added a protected addcar page using POST request, testing functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import AddCarPage from "./pages/AddCarPage/AddCarPage";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -15,24 +16,32 @@ import Footer from "./components/Footer/Footer";
 import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <PrivateRoute>
+                            <HomePage />
+                        </PrivateRoute>
+                    }
+                />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route
+                    path="/addcar"
+                    element={
+                        <PrivateRoute>
+                            <AddCarPage />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
