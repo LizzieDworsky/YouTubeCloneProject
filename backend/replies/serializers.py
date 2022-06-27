caded3306e261d8d3242ca7470a66b860d584086
LizzieDworsky <amydworsky15@gmail.com>
2022-06-27 10:51:28 -0600

    added all the component and page folders and files, did not add code to them yet

--- backend/replies/serializers.py
+++ backend/replies/serializers.py
@@ -1,9 +1,10 @@
-from dataclasses import fields
 from rest_framework import serializers
 from .models import Reply
 
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
-        fields = ["id", "text", "user_id", "comment"]
+        fields = ["id", "text", "user_id", "comment", "comment_id"]
         depth = 1
+
+    comment_id = serializers.IntegerField(write_only=True)
