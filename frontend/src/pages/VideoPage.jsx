0e263cf11292d2d5d2c0393c0a13c776e706cc8b
LizzieDworsky <amydworsky15@gmail.com>
2022-06-28 12:29:45 -0600

    added embedded video to VideoPage + imported Mapping component and used YouTube API to access 3 similar videos and mapped them on VideoPage

--- frontend/src/pages/VideoPage.jsx
+++ frontend/src/pages/VideoPage.jsx
@@ -1,9 +1,41 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 
+import Mapping from "../components/Mapping";
+
 const VideoPage = () => {
+    const [similarVideos, setSimilarVideos] = useState([]);
+
+    useEffect(() => {
+        getSimilarVideos();
+    }, []);
+
+    async function getSimilarVideos() {
+        try {
+            let response = await axios.get(
+                "https://www.googleapis.com/youtube/v3/search?relatedToVideoId=Shl9yb5DMao&type=video&key=AIzaSyDmDC6WaZZyIWfcF-Mi5tVZ3AruJlNDRKM&part=snippet&maxResults=3"
+            );
+            console.log(response.data);
+            setSimilarVideos(response.data.items);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+        console.log(similarVideos);
+    }
+
     return (
         <div>
-            <h1>This is the Video Page</h1>
+            <iframe
+                id="ytplayer"
+                type="text/html"
+                width="640"
+                height="360"
+                src="https://www.youtube.com/embed/M7lc1UVf-VE"
+                frameBorder="0"
+            ></iframe>
+            <div>
+                <Mapping array={similarVideos} />
+            </div>
         </div>
     );
 };
